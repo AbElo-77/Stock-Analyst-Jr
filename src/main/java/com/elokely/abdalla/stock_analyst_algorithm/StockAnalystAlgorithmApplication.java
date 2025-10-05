3a1cc07b1ca198239be6afd5b0abb1cf3a72b40f
AbElo-77 <abdallaelokely@gmail.com>
2025-10-04 23:37:55 -0600

    Initial Commit

---
+++ src/main/java/com/elokely/abdalla/stock_analyst_algorithm/StockAnalystAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.elokely.abdalla.stock_analyst_algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalystAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalystAlgorithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elokely/abdalla/stock_analyst_algorithm/StockAnalystAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elokely.abdalla.stock_analyst_algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAnalystAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
