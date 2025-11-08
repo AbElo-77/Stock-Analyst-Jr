18f1056cceeee237fa755932655ea50f587c401a
AbElo-77 <abdallaelokely@gmail.com>
2025-11-08 15:31:44 -0700

    Stock Data Compiled

--- src/main/java/com/elokely/abdalla/stock_analyst_algorithm/data/rawData/coreData/fetchCoreData.java
+++ src/main/java/com/elokely/abdalla/stock_analyst_algorithm/data/rawData/coreData/fetchCoreData.java
@@ -11,14 +11,16 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 
-// Derive: Volume (VWAP, VSMA, OBV), D/E Ratio
-
 public class FetchCoreData {
     private static final String API_KEY = ""; 
     private static final String API_SYMBOL = "AMZN"; 
 
     public static void main(String[] args) throws Exception {
         GETIntraday(12);
+        GETVWAP(12); 
+        GETOBV(12); 
+
+        GETOverview(); 
         GETDaily(); 
 
         int curMonth = 10; 
@@ -28,35 +30,57 @@ public class FetchCoreData {
         FileReader currFile = new FileReader("src\\main\\java\\com\\elokely\\abdalla\\stock_analyst_algorithm\\data\\rawData\\coreData\\AMZN_INTRADAY-" + yearCounter + monthString + ".csv"); 
         BufferedReader currReader = new BufferedReader(currFile);
 
-        // Getting Volume Calculations Based on Data
-        while (currReader != null) {
+        FileOutputStream outputReader = new FileOutputStream(API_SYMBOL + "_VSMA-" + yearCounter + monthString + ".csv"); 
 
-            String line; 
-            while ((line = currReader.readLine()) != null) {
+        for (int i = 0; i < 11; i++) {
 
-                // Process Information
-                String[] columns = line.split(","); 
-                System.out.println(columns[5]); 
+            // Getting Volume Calculations Based on Data
+            while (currReader != null) {
+
+                String line; 
+                int runningTotal = 0; 
+                int counter = 0; 
+                while ((line = currReader.readLine()) != null) {
+
+                    String[] columns = line.split(","); 
+                    if (line.contains("volume") || line.contains("Volume")) continue; 
+
+                    runningTotal += Integer.parseInt(columns[5]); 
+                    counter++;
+
+                    if (counter == 20) {
+                        String result = Integer.toString(runningTotal/20) + "\n";
+                        outputReader.write(result.getBytes());
+
+                        runningTotal = 0; 
+                        counter = 0; 
+                    }
+                }
+                curMonth--; 
+                if (curMonth > 9) monthString = "" + curMonth; else monthString = "0" + curMonth;
+
+                if (curMonth == 0) {
+                    yearCounter--; 
+                    curMonth = 12;
+                    monthString = "12";  
+                }
+
+                try { 
+
+                    currFile = new FileReader("src\\main\\java\\com\\elokely\\abdalla\\stock_analyst_algorithm\\data\\rawData\\coreData\\AMZN_INTRADAY-" + yearCounter + monthString + ".csv"); 
+                    currReader = new BufferedReader(currFile); 
+
+                    outputReader = new FileOutputStream(API_SYMBOL + "_VSMA-" + yearCounter + monthString + ".csv"); 
+                } catch (IOException e) {
+                    break; 
+                }
             }
 
-            currReader.close(); 
-            curMonth--; 
-            if (curMonth > 9) monthString = "" + curMonth; else monthString = "0" + curMonth;
-
-            if (curMonth == 0) {
-                yearCounter--; 
-                curMonth = 12;
-                monthString = "12";  
-            }
-
-            try { 
-                currFile = new FileReader("src\\main\\java\\com\\elokely\\abdalla\\stock_analyst_algorithm\\data\\rawData\\coreData\\AMZN_INTRADAY-" + yearCounter + monthString + ".csv"); 
-                currReader = new BufferedReader(currFile); }
-            catch (IOException e) {
-                break; 
-            }
+            
         }
-        
+
+        if (currReader != null) currReader.close(); 
+        outputReader.close();
 
     }
 
@@ -64,7 +88,7 @@ public class FetchCoreData {
 
         int curMonth = 1; 
         String monthString = "01"; 
-        int yearCounter = 2024; 
+        int yearCounter = 2025; 
 
         for (int i = 0; i < numRequests; i++) {
 
@@ -112,6 +136,156 @@ public class FetchCoreData {
                 yearCounter--; 
                 curMonth %= 12; 
             }
+        }
+    }
+
+        public static void GETVWAP(int numRequests) { 
+
+        int curMonth = 1; 
+        String monthString = "01"; 
+        int yearCounter = 2025; 
+
+        for (int i = 0; i < numRequests; i++) {
+
+            String alphaVantageAPI = "https://www.alphavantage.co/query?function=VWAP&";
+            URI alphaVantageURI = java.net.URI.create(alphaVantageAPI + 
+                                                      "symbol=" + API_SYMBOL 
+                                                      + "&interval=5min&month="
+                                                      + yearCounter + "-" + monthString 
+                                                      + "&apikey=" + API_KEY 
+                                                      + "&datatype=csv"); 
+            String fileName = API_SYMBOL + "_VWAP-" + yearCounter + monthString + ".csv"; 
+
+                try { 
+
+                    URL alphaVantageURL = alphaVantageURI.toURL(); 
+                    HttpURLConnection getConnection = (HttpURLConnection) alphaVantageURL.openConnection(); 
+
+                    getConnection.setRequestMethod("GET"); 
+                    int responseStatus = getConnection.getResponseCode(); 
+
+                    if (responseStatus ==  HttpURLConnection.HTTP_OK) {
+                        
+                        InputStream inputReader = getConnection.getInputStream();
+                        FileOutputStream outputReader = new FileOutputStream(fileName); 
+
+                        byte[] buffer = new byte[8192];
+                        int bytesRead;
+                        while ((bytesRead = inputReader.read(buffer))!= -1) {
+                            outputReader.write(buffer, 0, bytesRead);
+                        }
+                        inputReader.close(); 
+                        outputReader.close(); 
+
+                    } else {
+                        throw new Error("Error fetching data from Alpha Vantage!"); 
+                    } 
+                } catch (IllegalArgumentException | IOException e) {
+                    throw new RuntimeException("URL not accessible."); 
+                }
+
+            curMonth++; 
+            if (curMonth > 9) monthString = "" + curMonth; else monthString = "0" + curMonth;
+
+            if (curMonth > 12) {
+                yearCounter--; 
+                curMonth %= 12; 
+            }
+        }
+    }
+
+            public static void GETOBV(int numRequests) { 
+
+        int curMonth = 1; 
+        String monthString = "01"; 
+        int yearCounter = 2025; 
+
+        for (int i = 0; i < numRequests; i++) {
+
+            String alphaVantageAPI = "https://www.alphavantage.co/query?function=OBV&";
+            URI alphaVantageURI = java.net.URI.create(alphaVantageAPI + 
+                                                      "symbol=" + API_SYMBOL 
+                                                      + "&interval=daily&month="
+                                                      + yearCounter + "-" + monthString 
+                                                      + "&apikey=" + API_KEY 
+                                                      + "&datatype=csv"); 
+            String fileName = API_SYMBOL + "_OBV-" + yearCounter + monthString + ".csv"; 
+
+                try { 
+
+                    URL alphaVantageURL = alphaVantageURI.toURL(); 
+                    HttpURLConnection getConnection = (HttpURLConnection) alphaVantageURL.openConnection(); 
+
+                    getConnection.setRequestMethod("GET"); 
+                    int responseStatus = getConnection.getResponseCode(); 
+
+                    if (responseStatus ==  HttpURLConnection.HTTP_OK) {
+                        
+                        InputStream inputReader = getConnection.getInputStream();
+                        FileOutputStream outputReader = new FileOutputStream(fileName); 
+
+                        byte[] buffer = new byte[8192];
+                        int bytesRead;
+                        while ((bytesRead = inputReader.read(buffer))!= -1) {
+                            outputReader.write(buffer, 0, bytesRead);
+                        }
+                        inputReader.close(); 
+                        outputReader.close(); 
+
+                    } else {
+                        throw new Error("Error fetching data from Alpha Vantage!"); 
+                    } 
+                } catch (IllegalArgumentException | IOException e) {
+                    throw new RuntimeException("URL not accessible."); 
+                }
+
+            curMonth++; 
+            if (curMonth > 9) monthString = "" + curMonth; else monthString = "0" + curMonth;
+
+            if (curMonth > 12) {
+                yearCounter--; 
+                curMonth %= 12; 
+            }
+        }
+    }
+
+    public static void GETOverview() { 
+
+
+        String alphaVantageAPI = "https://www.alphavantage.co/query?function=OVERVIEW&";
+        URI alphaVantageURI = java.net.URI.create(alphaVantageAPI + 
+                                                      "symbol=" + API_SYMBOL 
+                                                      + "&apikey=" + API_KEY
+                                                      + "&datatype=csv"); 
+        String fileName = API_SYMBOL + "_DAILY-" + ".csv"; 
+
+        try { 
+
+            URL alphaVantageURL = alphaVantageURI.toURL(); 
+            HttpURLConnection getConnection = (HttpURLConnection) alphaVantageURL.openConnection(); 
+
+            getConnection.setRequestMethod("GET"); 
+            int responseStatus = getConnection.getResponseCode(); 
+
+            if (responseStatus ==  HttpURLConnection.HTTP_OK) {
+                        
+                InputStream inputReader = getConnection.getInputStream();
+                FileOutputStream outputReader = new FileOutputStream(fileName); 
+
+                byte[] buffer = new byte[8192];
+                int bytesRead;
+                while ((bytesRead = inputReader.read(buffer))!= -1) {
+                        outputReader.write(buffer, 0, bytesRead);
+                }
+
+                inputReader.close(); 
+                outputReader.close(); 
+
+            } else {
+                throw new Error("Error fetching data from Alpha Vantage!"); 
+            } 
+        } catch (IllegalArgumentException | IOException e) {
+            throw new RuntimeException("URL not accessible."); 
         }
     }
 
--- src/main/java/com/elokely/abdalla/stock_analyst_algorithm/data/rawData/indicatorsData/fetchIndicators.java
+++ src/main/java/com/elokely/abdalla/stock_analyst_algorithm/data/rawData/indicatorsData/fetchIndicators.java
@@ -7,8 +7,6 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 
-// External: CBOE Volatility Index (VIX), Consumer Price Index (CPI)
-
 public class FetchIndicators {
     private static final String API_KEY = ""; 
     private static final String API_SYMBOL = "AMZN"; 
@@ -26,11 +24,11 @@ public class FetchIndicators {
 
         GETStoch(12); 
         GETRSI(12);
-
         GETAroon(12);
         GETBBands(12);
 
-
+        GETCPI(); 
+        GETGDP(); 
     }
 
         public static void GETSMA_ShortTerm(int numRequests) { 
@@ -44,7 +42,7 @@ public class FetchIndicators {
             String alphaVantageAPI = "https://www.alphavantage.co/query?function=SMA&";
             URI alphaVantageURI = java.net.URI.create(alphaVantageAPI + 
                                                       "symbol=" + API_SYMBOL 
-                                                      + "&interval=5min&month="
+                                                      + "&interval=daily&month="
                                                       + yearCounter + "-" + monthString 
                                                       + "&time_period=500&series_type=open&apikey=" 
                                                       + API_KEY + "&datatype=csv"); 
@@ -100,7 +98,7 @@ public class FetchIndicators {
             String alphaVantageAPI = "https://www.alphavantage.co/query?function=EMA&";
             URI alphaVantageURI = java.net.URI.create(alphaVantageAPI + 
                                                       "symbol=" + API_SYMBOL 
-                                                      + "&interval=5min&month="
+                                                      + "&interval=daily&month="
                                                       + yearCounter + "-" + monthString 
                                                       + "&time_period=500&series_type=open&apikey=" 
                                                       + API_KEY + "&datatype=csv"); 
@@ -210,7 +208,7 @@ public class FetchIndicators {
             String alphaVantageAPI = "https://www.alphavantage.co/query?function=EMA&";
             URI alphaVantageURI = java.net.URI.create(alphaVantageAPI + 
                                                       "symbol=" + API_SYMBOL 
-                                                      + "&interval=daily&month="
+                                                      + "&interval=weekly&month="
                                                       + yearCounter + "-" + monthString 
                                                       + "&time_period=500&series_type=open&apikey=" 
                                                       + API_KEY + "&datatype=csv"); 
@@ -265,7 +263,7 @@ public class FetchIndicators {
             String alphaVantageAPI = "https://www.alphavantage.co/query?function=MACD&";
             URI alphaVantageURI = java.net.URI.create(alphaVantageAPI + 
                                                       "symbol=" + API_SYMBOL 
-                                                      + "&interval=daily&month="
+                                                      + "&interval=weekly&month="
                                                       + yearCounter + "-" + monthString 
                                                       + "&series_type=open&apikey=" 
                                                       + API_KEY + "&datatype=csv"); 
@@ -583,7 +581,6 @@ public class FetchIndicators {
             }
         }
         }
-
         
         public static void GETATR(int numRequests) { 
 
@@ -638,5 +635,83 @@ public class FetchIndicators {
                 curMonth %= 12; 
             }
         }
+        }
+
+        public static void GETCPI() { 
+
+            String alphaVantageAPI = "https://www.alphavantage.co/query?function=CPI&";
+            URI alphaVantageURI = java.net.URI.create(alphaVantageAPI + 
+                                                      "symbol=" + API_SYMBOL 
+                                                      + "&interval=monthly"
+                                                      + "&apikey=" + API_KEY
+                                                      + "&datatype=csv"); 
+            String fileName = API_SYMBOL + "_CPI-" + ".csv"; 
+
+            try { 
+
+                URL alphaVantageURL = alphaVantageURI.toURL(); 
+                HttpURLConnection getConnection = (HttpURLConnection) alphaVantageURL.openConnection(); 
+
+                getConnection.setRequestMethod("GET"); 
+                int responseStatus = getConnection.getResponseCode(); 
+
+                if (responseStatus ==  HttpURLConnection.HTTP_OK) {
+                        
+                    InputStream inputReader = getConnection.getInputStream();
+                    FileOutputStream outputReader = new FileOutputStream(fileName); 
+
+                    byte[] buffer = new byte[8192];
+                    int bytesRead;
+                    while ((bytesRead = inputReader.read(buffer))!= -1) {
+                        outputReader.write(buffer, 0, bytesRead);
+                    }
+                    inputReader.close(); 
+                    outputReader.close(); 
+
+                } else {
+                    throw new Error("Error fetching data from Alpha Vantage!"); 
+                } 
+            } catch (IllegalArgumentException | IOException e) {
+                throw new RuntimeException("URL not accessible."); 
+            }
+        }
+
+        public static void GETGDP() { 
+
+            String alphaVantageAPI = "https://www.alphavantage.co/query?function=REAL_GDP&";
+            URI alphaVantageURI = java.net.URI.create(alphaVantageAPI + 
+                                                      "symbol=" + API_SYMBOL 
+                                                      + "&interval=quarterly"
+                                                      + "&apikey=" + API_KEY
+                                                      + "&datatype=csv"); 
+            String fileName = API_SYMBOL + "_GDP-" + ".csv"; 
+
+            try { 
+
+                URL alphaVantageURL = alphaVantageURI.toURL(); 
+                HttpURLConnection getConnection = (HttpURLConnection) alphaVantageURL.openConnection(); 
+
+                getConnection.setRequestMethod("GET"); 
+                int responseStatus = getConnection.getResponseCode(); 
+
+                if (responseStatus ==  HttpURLConnection.HTTP_OK) {
+                        
+                    InputStream inputReader = getConnection.getInputStream();
+                    FileOutputStream outputReader = new FileOutputStream(fileName); 
+
+                    byte[] buffer = new byte[8192];
+                    int bytesRead;
+                    while ((bytesRead = inputReader.read(buffer))!= -1) {
+                        outputReader.write(buffer, 0, bytesRead);
+                    }
+                    inputReader.close(); 
+                    outputReader.close(); 
+
+                } else {
+                    throw new Error("Error fetching data from Alpha Vantage!"); 
+                } 
+            } catch (IllegalArgumentException | IOException e) {
+                throw new RuntimeException("URL not accessible."); 
+            }
         }
 }
